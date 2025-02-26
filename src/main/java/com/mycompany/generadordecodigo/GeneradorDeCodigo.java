00d69d3d329d510b10976017353e2c08e5a7d3dd
Ezequiel <ezequielmazariegossolis@gmail.com>
2025-02-26 11:09:33 -0600

    Inicio del proyecto maven

---
+++ src/main/java/com/mycompany/generadordecodigo/GeneradorDeCodigo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.generadordecodigo;
+
+/**
+ *
+ * @author ezequielpena
+ */
+public class GeneradorDeCodigo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
