fbca6342f888c3c727114ed1ff3dd0db525aef8d
Ezequiel <ezequielmazariegossolis@gmail.com>
2025-03-18 21:17:31 -0600

    archivo con la base de datos (ya no será necesario)

---
+++ src/main/java/filesutils/codegenerator.sql
@@ -0,0 +1,23 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  mariormoreno & ezequiel
+ * Created: Mar 6, 2025
+ */
+
+-- Cami2010/07
+CREATE DATABASE codegenerator;
+\c codegenerator
+
+CREATE TABLE databases
+(
+    id SERIAL PRIMARY KEY,
+    ip VARCHAR(25),
+    port VARCHAR(15),
+    database_name VARCHAR(40),
+    user_name VARCHAR(40),
+    password_db VARCHAR(50)    
+);
+
